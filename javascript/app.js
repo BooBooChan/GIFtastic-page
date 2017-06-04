e199485f10c49fdf11a4cb7772b13d3c5835afb0
Bridget Bourne <Bridget Bourne>
2017-06-03 21:15:09 -0400

    Basic structure and pseudocoding

---
+++ javascript/app.js
@@ -0,0 +1,36 @@
+//REMEMBER TO CONSOLE.LOG EVERYTHING! LEAVE NO STONE UNTURNED!
+
+//Step 1: Create AJAX code which will receive API data from server and send queries to server.
+ var queryURL = "";//insert url here
+ var apiPublicKey = "dc6zaTOxFJmzC";
+
+ $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+//Input your JSON and API parameters inside this entire object.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      console.log(response);
+    });
+
+    //Public API key: dc6zaTOxFJmzC
+
+//Step 2: 
